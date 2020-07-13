d44ec37b789e82227e2363ab9dcd3062fa00e062
TimothyTedaldi <TimothyTedaldi@users.noreply.github.com>
2020-07-13 16:48:45 +0200

    added header

---
+++ one_pager_pwa/src/components/header.jsx
@@ -0,0 +1,26 @@
+<div>
+<section id="top" class="one dark cover">
+                            <div class="container">
+
+                    <header>
+                        <h1><span style="font-size: 3em;">
+                            <Strong>Bienvenue, chez Les Amis des Animaux, <br>
+                            une 
+                            <a href="https://fr.wikipedia.org/wiki/Association_sans_but_lucratif">a.s.b.l.</a> au service de nos animaux<br></Strong>
+                            </span><br><br>
+                    </h1>
+                    <blockquote>
+                    <p>
+                        <a href="https://citation-celebre.leparisien.fr/citations/23983">"Les animaux ont des droits :<br>
+                        le droit d'être protégés par l'homme, le droit à la vie et à la multiplication de l'espèce, <br>
+                        le droit à la liberté et le droit de n'avoir aucune dette envers l'homme."</a>
+                    </p>
+                    <p style="text-align:right">
+                        <a href="https://en.wikipedia.org/wiki/Luther_Standing_Bear">Luther Standing Bear</a><br>
+                    </p>
+                </blockquote>
+
+            </header>
+        </div>
+    </section>
+</div>
